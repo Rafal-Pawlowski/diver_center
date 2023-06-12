a69ed7fd02cfb691f5b64a2c0372eaedcfc50611
Rafal <rafalpawlowski.poczta@gmail.com>
2023-06-12 19:32:47 +0200

    Initial commit

---
+++ src/main/java/com/diver/center/diver_center/DiverCenterApplication.java
@@ -0,0 +1,13 @@
+package com.diver.center.diver_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiverCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiverCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diver/center/diver_center/DiverCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diver.center.diver_center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiverCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
